c8d00c8e1ff4af34f6d1b11d27ea358d44d241a1
Alaa Elsisy <elsisyalaa0@gmail.com>
2026-03-14 16:46:23 +0200

    edit

--- E-Learning.Core/Interfaces/Repositories/Assessments/Exams/IExamRepository.cs
+++ E-Learning.Core/Interfaces/Repositories/Assessments/Exams/IExamRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace E_Learning.core.Interfaces.Repositories.Assessments.Exams
+﻿using E_Learning.Core.Entities.Assessments.Exams;
+using E_Learning.Core.Interfaces.Repositories;
+
+namespace E_Learning.core.Interfaces.Repositories.Assessments.Exams
 {
-    public interface IExamRepository
+    public interface IExamRepository:IGenericRepository<Exam,int>
     {
+        Task<IEnumerable<Exam>> GetExamsByCourseIdAsync(int courseId);
     }
 }
--- E-Learning.Repository/Repositories/GenericesRepositories/Assessments/Exams/ExamRepository.cs
+++ E-Learning.Repository/Repositories/GenericesRepositories/Assessments/Exams/ExamRepository.cs
@@ -1,15 +1,23 @@
 ﻿using E_Learning.core.Interfaces.Repositories.Assessments.Exams;
+using E_Learning.Core.Entities.Assessments.Exams;
 using E_Learning.Repository.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Learning.Repository.Repositories.GenericesRepositories.Assessments.Exams
 {
-    public class ExamRepository :IExamRepository
+    public class ExamRepository :GenericRepository<Exam,int>, IExamRepository
     {
-        public ExamRepository(ELearningDbContext context)
+        public ExamRepository(ELearningDbContext context):base(context)
         {
             _context = context;
         }
 
         public ELearningDbContext _context { get; }
+
+        public async Task<IEnumerable<Exam>> GetExamsByCourseIdAsync(int courseId)
+        {
+            return await _context.Exams.Where(e => e.CourseId == courseId)
+                .Include(e => e.Course).ToListAsync();
+        }
     }
 }
